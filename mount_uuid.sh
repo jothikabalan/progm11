f0fd8d90a600120073904f95139fd3a9e6fdfffe
jothikabalan <jothirioriojothi@gmail.com>
2026-09-16 12:20:56 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -2,27 +2,28 @@
 
 # ==========================================
 # Mount Filesystem Using UUID
-# Student Name:
-# Roll Number:
+# Student Name:Jothika
+# Roll Number:1U24IT047
 # ==========================================
 
 
 # Display filesystem UUID
-
+sudo blkid
 
 
 
 # Create mount directory
-
+sudo mkdir -p /mnt/mydisk
 
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
+sudo mount UUID=YOUR_UUID /mnt/mydisk
 
 
 
 
 # Display mounted filesystem
-
+df -h
 
